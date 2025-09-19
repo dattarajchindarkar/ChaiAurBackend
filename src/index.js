80408b134d8f565e99794589629b518943974e28
dattarajchindarkar <dattarajchindarkar30@gmail.com>
2025-09-19 11:35:17 +0530

    setting up the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
 
 // Load environment variables
 dotenv.config({ path: "./.env" });
 
+//creating express instance
+const app = express();
+
+//sending a plain text to client
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
 // Connect to DB
 connectDB();
 
-// Start server later (Express will go here)
+//const PORT = process.env.PORT || 3000;
+
+//starting server
+app.listen(PORT, () => {
+  console.log(`sever is running at http://localhost:${process.env.PORT}`);
+});
